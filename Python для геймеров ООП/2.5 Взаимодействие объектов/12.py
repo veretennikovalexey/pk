fdb9389d6578de133f18006ed01f6d5bd9836138
veretennikovalexey <raidex@yandex.ru>
2025-11-16 11:10:04 +0300

    alex

---
+++ Python для геймеров ООП/2.5 Взаимодействие объектов/12.py
@@ -0,0 +1,9 @@
+class Plant:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        
+class Zombie:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
