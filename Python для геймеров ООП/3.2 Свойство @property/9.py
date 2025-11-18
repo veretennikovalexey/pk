9ec040452f773add2fbec8c0efbfecf8f521fe32
veretennikovalexey <raidex@yandex.ru>
2025-11-19 00:32:18 +0300

    alex

---
+++ Python для геймеров ООП/3.2 Свойство @property/9.py
@@ -0,0 +1,11 @@
+class Hero:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+    
+hero = Hero("Зяблик")
+print(hero.name)
+hero.name = "KillByaz"
