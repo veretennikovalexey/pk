0eea7fe09fd04a354f56bb5f926ed95bef5942f8
veretennikovalexey <raidex@yandex.ru>
2025-12-03 07:58:59 +0300

    alex

---
+++ Python для геймеров ООП/6.2 Агрегация объектов/09.py
@@ -0,0 +1,37 @@
+class Ammo:
+    '''Пиф-паф! Ой-ой-ой! Часть 1'''
+    def __init__(self, count):
+        self.count = count
+
+    def use(self):
+        self.count -= 1
+        self.count = max(self.count, 0)
+        if self.count == 0:
+            print('Патроны закончились!')
+        else:
+            print(f'Остаток патронов: {self.count}') 
+
+
+class Weapon:
+    '''Пиф-паф! Ой-ой-ой! Часть 2'''
+    def __init__(self, name, ammo):
+        self.name = name
+        self.ammo = ammo
+
+    def shoot(self):
+        if self.ammo and self.ammo.count > 0:
+            print(f'{self.name} делает выстрел!')
+            self.ammo.use()
+        else:
+            print('Нет патронов!')
+
+
+# Пиф-паф! Ой-ой-ой! Часть 3
+count = int(input())
+name = input()
+shots = int(input())
+
+ammo = Ammo( count )
+gun = Weapon( name, ammo )
+for _ in range( shots ):
+   gun.shoot() 
