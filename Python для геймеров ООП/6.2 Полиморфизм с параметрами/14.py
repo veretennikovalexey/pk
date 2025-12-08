0fd4e775871fe824b153c7e7615fe883ad29a8ee
veretennikovalexey <raidex@yandex.ru>
2025-12-08 12:23:36 +0300

    alex

---
+++ Python для геймеров ООП/6.2 Полиморфизм с параметрами/14.py
@@ -0,0 +1,13 @@
+class Zombie:
+    def attack(self, power): 
+        print(f'Зомби бьёт игрока на {power} урона.')
+
+
+class Skeleton: 
+    def attack(self, power): 
+        print(f'Скелет выпускает стрелу на {power} урона.')
+
+
+class Creeper:
+    def attack(self, power): 
+        print(f'Крипер взрывается с силой {power}!')
