6162f6efd71a6cd37eaa6df4c4c47425c7c06957
veretennikovalexey <raidex@yandex.ru>
2025-12-09 16:22:28 +0300

    alex

---
+++ Осознанный Python — ООП/07.py
@@ -0,0 +1,8 @@
+class Person:
+    name = "Vasya"
+    time = 2
+
+
+message = f"Теперь {Person.name} может программировать {Person.time} часа без перерыва"
+print(message)
+
---
+++ Осознанный Python — ООП/09.py
@@ -0,0 +1,7 @@
+class Backpack:
+    things = ["хомяк", "капустка", "фонарик",
+              "шапка Пикачу", "игрушечный меч майнкрафт"]
+
+
+for thing in Backpack.things:
+    print(thing)
